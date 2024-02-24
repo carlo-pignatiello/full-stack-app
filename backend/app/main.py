57f4b098d5a7f908774e0dfb78e010dbc06e5f8d
eelpigna <carlopigna95@gmail.com>
2024-02-24 22:35:37 +0100

    docker setup for web server

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from routes import user_router
-import uvicorn
+# import uvicorn
 
 app = FastAPI()
 
 app.include_router(user_router)
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app",host='0.0.0.0', port=4557, reload=True, workers=4)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app",host='0.0.0.0', port=4557, reload=True, workers=4)
